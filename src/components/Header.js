5563874ec450e2245896c2e9237c25a96a4ac3fb
muhammed seydali <60098100+muhammedseydali@users.noreply.github.com>
2022-09-08 14:03:58 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className='display-flex'>
+      <div className='logo'>Logo </div>
+      <ul className="nav display-flex">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
